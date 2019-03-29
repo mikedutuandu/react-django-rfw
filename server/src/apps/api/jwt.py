91cc468736ff096e8385032da9aed977431f806d
tuan.dinh <tuan.dinh@groovetechnology.com>
2019-03-29 09:45:56 +0700

    add function create manual jwt token

--- server/src/apps/api/jwt.py
+++ server/src/apps/api/jwt.py
@@ -3,8 +3,21 @@ from .serializers import (
     UserDetailSerializer
     )
 
+from rest_framework_jwt.settings import api_settings
+
 def jwt_response_payload_handler(token, user=None, request=None):
     return {
         'token': token,
         'user': UserDetailSerializer(user, context={'request': request}).data
     }
+
+def create_jwt_token_from_user(user):
+
+    jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+
+    jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+
+    payload = jwt_payload_handler(user)
+    token = jwt_encode_handler(payload)
+
+    return token
